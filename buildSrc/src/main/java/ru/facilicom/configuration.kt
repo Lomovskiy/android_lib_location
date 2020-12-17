9d365ce283e618c341e5cae10e61b9d50e5b1e8d
Alexander Lomovskiy <a@lomovskiy.com>
2020-12-17 22:31:40 +0300

    Refactored deps

--- buildSrc/src/main/java/ru/facilicom/configuration.kt
+++ buildSrc/src/main/java/ru/facilicom/configuration.kt
@@ -3,8 +3,10 @@ object Config {
     object Deps {
 
         private const val kotlinVersion = "1.4.21"
+        private const val materialDesignComponentsVersion = "1.2.1"
 
         const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
+        const val materialDesignComponents = "com.google.android.material:material:$materialDesignComponentsVersion"
 
     }
 

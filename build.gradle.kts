8cbe1dbab99916a60fd408bd04a33f0ae6372d57
Alexander Lomovskiy <a@lomovskiy.com>
2020-12-17 21:38:03 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+buildscript {
+
+    repositories {
+        google()
+        jcenter()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.1.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+    }
+
+}
+
+allprojects {
+
+    repositories {
+        google()
+        jcenter()
+    }
+
+}
